68eeb80f953b8a1eac1a9649e1e93cf72d39396b
raunakbhagat <rabhagat31@gmail.com>
2020-04-13 20:37:07 -0600

    moved method defintiions to separate file.

--- src/BaseState/BaseState.cpp
+++ src/BaseState/BaseState.cpp
@@ -145,6 +145,8 @@
 BaseState::Jumper::Jumper() { return; }
 BaseState::Jumper::Jumper(Jumper const & other) { return; }
 void BaseState::Jumper::operator=(Jumper const & other) { return; }
+BaseState::BaseState(BaseState const & other) { return; }
+void BaseState::operator=(BaseState const & other) { return; }
 
 
 
@@ -156,6 +158,70 @@ bool const BaseState::Jumper::operator!=(BaseState::Jumper const & other) const 
 
 std::string const BaseState::Jumper::getAction() const { return this->action; }
 std::shared_ptr<BaseState> const BaseState::Jumper::getPtr() const { return this->ptr; }
+
+
+
+unsigned int BaseState::counter = 0;
+
+BaseState::BaseState() { return; }
+BaseState::~BaseState() {
+    typename std::vector<BaseState::Jumper const *>::iterator itr(this->jumpers->begin());
+    for (; itr!=this->jumpers->end(); ++itr) delete *itr;
+
+    delete jumpers;
+    return;
+}
+
+bool const BaseState::operator==(BaseState const & other) const { return this->id == other.getID(); }
+bool const BaseState::operator!=(BaseState const & other) const { return !this->operator==(other); }
+
+int const BaseState::getID() const { return this->id; }
+
+bool const BaseState::containsAction(std::string const & action) const {
+    typename std::vector<BaseState::Jumper const *>::iterator itr(this->jumpers->begin());
+    for (; itr!=this->jumpers->end(); ++itr) if ((*itr)->getAction() == action) return true;
+
+    return false;
+}
+bool const BaseState::containsBaseState(std::shared_ptr<BaseState> const & baseState) const {
+    if (!baseState) return false;
+
+    typename std::vector<BaseState::Jumper const *>::iterator itr(this->jumpers->begin());
+    for (; itr!=this->jumpers->end(); ++itr) if ((*itr)->getBaseState() == baseState) return true;
+
+    return false;
+}
+bool const BaseState::addJumper(std::string const && action, std::shared_ptr<BaseState> const baseState) {
+    if (!baseState || this->containsAction(action) || this->containsBaseState(baseState)) return false;
+    this->jumpers->push_back(new BaseState::Jumper(action,baseState));
+
+    return true;
+}
+bool const BaseState::removeJumper(std::string const & action) {
+    typename std::vector<BaseState::Jumper const *>::iterator itr_a(this->jumpers->begin());
+    typename std::vector<BaseState::Jumper const *>::iterator itr_b(itr_a);
+    BaseState::Jumper const * temp(nullptr);
+
+    for (; itr_a!=this->jumpers->end(); ++itr_a) {
+        if ((*itr_a)->getAction() != action) {
+            *itr_b = *itr_a;
+            ++itr_b;
+        } else {
+            delete *itr_a;
+            *itr_a = nullptr;
+        }
+    }
+    this->jumpers->erase(itr_b,itr_a);
+
+    return false;
+}
+
+std::shared_ptr<BaseState> const BaseState::transition(std::string const & action) const {
+    typename std::vector<BaseState::Jumper const *>::iterator itr(this->jumpers->begin());
+    for (; itr!=this->jumpers->end(); ++itr) if ((*itr)->getAction() == action) return (*itr)->getBaseState();
+    
+    return nullptr;
+}
 
 
 int main() {
